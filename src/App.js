eb269cd6610e50d7de244226255b2b74ffef3487
braojntu <braojntu@gmail.com>
2022-02-13 22:01:59 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ function App() {
     const response = await axios.get(
       "https://www.anapioficeandfire.com/api/books?pageSize=30"
     );
-
     setBooks(response.data);
   };
 
   return (
     <div className="App">
       <h1>Game of Thrones Books</h1>
-      <h2>Fetch API Data and display it - 1 of 3</h2>
-
+      <h2>(Fetch API data and display using React - Project 1)</h2>
       {/* Fetch data from API */}
       <div>
         <button className="fetch-button" onClick={fetchData}>
@@ -25,21 +23,20 @@ function App() {
         </button>
         <br />
       </div>
-
       {/* Display data from API */}
-      <div className="books">
+      <div className="card-container">
         {books &&
           books.map((book, index) => {
             const cleanedDate = new Date(book.released).toDateString();
             const authors = book.authors.join(", ");
 
             return (
-              <div className="book" key={index}>
-                <div className="book-header">
+              <div className="card" key={index}>
+                <div className="card-title">
                   <h3>Book {index + 1}</h3>
                   <h2>{book.name}</h2>
                 </div>
-                <div className="book-details">
+                <div className="card-details">
                   <p>Author: {authors}</p>
                   <p>Pages: {book.numberOfPages}</p>
                   <p>Country: {book.country}</p>
